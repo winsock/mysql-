b2207562cc9ef25fe01b93de55bcc37ed58fa3bd
wyoung <wyoung@5946f024-35f4-0310-b1b3-9a3be3380cfb>
2006-03-10 21:07:13 +0000

    Minor changes: comment changes, whitespace changes, etc.

--- branches/v2.1-bakefile/lib/connection.h
+++ branches/v2.1-bakefile/lib/connection.h
@@ -10,7 +10,7 @@
 
 /***********************************************************************
  Copyright (c) 1998 by Kevin Atkinson, (c) 1999, 2000 and 2001 by
- MySQL AB, and (c) 2004, 2005 by Educational Technology Resources, Inc.
+ MySQL AB, and (c) 2004-2006 by Educational Technology Resources, Inc.
  Others may also hold copyrights on code in this file.  See the CREDITS
  file in the top directory of the distribution for details.
 
